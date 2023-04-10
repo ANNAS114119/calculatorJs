3cc487761697e21c02554da70ff4992b34280763
MOHAMMAD ANAS <86908654+ANNAS114119@users.noreply.github.com>
2023-04-10 16:48:15 +0530

    Add files via upload

---
+++ JavaScriptCalculator/main.js
@@ -0,0 +1,9 @@
+const input = document.getElementById('input').value
+const button = document.querySelector('btn')
+const equal = document.querySelector('btn-equal')
+
+button.onclick = () => {
+ 
+    button.innerHTML = input
+
+}
